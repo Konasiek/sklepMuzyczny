5cd6aa4ec0db63f0e85dbfa304c356837b92f410
Konrad Merkwa <konasiek@gmail.com>
2019-09-14 09:34:46 +0200

    end

--- src/main/java/com/sklepmuzyczny/demo/repository/ProductRepository.java
+++ src/main/java/com/sklepmuzyczny/demo/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.sklepmuzyczny.demo.repository;
 
 import com.sklepmuzyczny.demo.model.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
 

90b92987d3a73c2287318fcc24b0a53c83e2104e
Konasiek <konasiek@gmail.com>
2020-11-18 14:09:25 +0100

    added Jwt to angular - not working yet

--- angular/angularclient/src/app/app-routing.module.ts
+++ angular/angularclient/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ProductComponent} from "./components/product/product.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
-  {path: 'product', component: ProductComponent}
+  {path: 'product', component: ProductComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- angular/angularclient/src/app/app.module.ts
+++ angular/angularclient/src/app/app.module.ts
@@ -1,23 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {ProductComponent} from "./components/product/product.component";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
+import {LoginComponent} from './components/login/login.component';
+import {FormsModule} from "@angular/forms";
+import {SignupComponent} from "./components/signup/signup.component";
+import {CookieService} from "ngx-cookie-service";
+import {JwtInterceptor} from "./_interceptors/jwt-interceptor.service";
+import {ErrorInterceptor} from "./_interceptors/error-interceptor.service";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    LoginComponent,
+    SignupComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+
   ],
-  providers: [ProductService],
+  providers: [
+    CookieService,
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
